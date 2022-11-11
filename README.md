00be53760beebfc8839e1fc714fbff388b97fcaa
rcxxx <chuangxinr@gmail.com>
2022-11-11 16:22:52 +0800

    first test success

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Bullet_Chat_Hunter
 
 ## 基于 Python AioWebSocket 实现的弹幕爬虫工具
+
+
+
+## 参考
+- **[huihui486 / B站直播弹幕姬](https://gitee.com/huihui486/bilibili-danmuji)**
+- **[AioWebSocket实现python异步接收B站直播弹幕](https://blog.csdn.net/Sharp486/article/details/122466308)**
---
+++ main.py
@@ -0,0 +1,86 @@
+#  coding:utf-8
+
+import asyncio
+import zlib
+import json
+
+import serial
+
+from aiowebsocket.converses import AioWebSocket
+
+remote = 'ws://broadcastlv.chat.bilibili.com:2244/sub'
+heart_beat_msg = '00 00 00 10 00 10 00 01  00 00 00 02 00 00 00 01'
+heart_beat_interval = 30
+
+g_wooden_fish_num = 0
+
+ser = serial.Serial(port='/dev/ttyUSB0',
+                    baudrate=115200,
+                    timeout=0.5)
+
+def writeSer():
+    global g_wooden_fish_num
+    print(g_wooden_fish_num)
+    ser.write(str(g_wooden_fish_num).encode('utf-8'))
+    g_wooden_fish_num = (g_wooden_fish_num+1)%4
+
+async def startUp(_room_id):
+    data_raw = '000000{Header}0010000100000007000000017b22726f6f6d6964223a{ID}7d'
+    data_raw = data_raw.format(Header=hex(27 + len(_room_id))[2:],
+                                         ID=''.join(map(lambda x: hex(ord(x))[2:], list(_room_id))))
+
+    async with AioWebSocket(remote) as aws:
+        converse = aws.manipulator
+        await converse.send(bytes.fromhex(data_raw))
+        tasks = [receivePackage(converse), sendHearBeat(converse)]
+        await asyncio.wait(tasks)
+
+async def sendHearBeat(_webscoket):
+    while True:
+        await asyncio.sleep(heart_beat_interval)
+        await _webscoket.send(bytes.fromhex(heart_beat_msg))
+        print('[Notice] Sent HeartBeat.')
+
+async def receivePackage(_webscoket):
+    while True:
+        recv_text = await _webscoket.receive()
+
+        if recv_text == None:
+            recv_text = b'\x00\x00\x00\x1a\x00\x10\x00\x01\x00\x00\x00\x08\x00\x00\x00\x01{"code":0}'
+
+        parseData(recv_text)
+
+def parseData(_data):
+    # 获取数据包的长度，版本和操作类型
+    packet_len = int(_data[:4].hex(), 16)
+    ver = int(_data[6:8].hex(), 16)
+    op = int(_data[8:12].hex(), 16)
+
+    if (len(_data) > packet_len):
+        parseData(_data[packet_len:])
+        _data = _data[:packet_len]
+    if (ver == 2):
+        _data = zlib.decompress(_data[16:])
+        parseData(_data)
+        return
+    if (ver == 1):
+        return
+
+    if (op == 5):
+        try:
+            jd = json.loads(_data[16:].decode('utf-8', errors='ignore'))
+            if (jd['cmd'] == 'DANMU_MSG'):
+                print('[DANMU] ', jd['info'][2][1], ': ', jd['info'][1])
+
+                writeSer()
+        except Exception as e:
+            pass
+
+if __name__ == '__main__':
+
+    try:
+        room_id = ''
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(startUp(room_id))
+    except KeyboardInterrupt as exc:
+        print('Quit.')
